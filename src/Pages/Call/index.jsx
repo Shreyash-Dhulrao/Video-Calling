86dfa5e8b3a5fcea6aa37b06e71c7886210d1709
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2024-11-15 21:20:03 +0530

    resolution update

--- src/Pages/Call/index.jsx
+++ src/Pages/Call/index.jsx
@@ -4,7 +4,7 @@ import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 import { nanoid } from 'nanoid'
 
 const Call = () => {
-    const  idData = useParams()
+    const idData = useParams()
     console.log(idData)
     const nan = nanoid(5)
 
@@ -13,23 +13,38 @@ const Call = () => {
         const serverSecret = "bfbc2aa7e6697dc7a2bdf0634254b7ff"
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, idData.id, nan.toString(), "Shreyash");
         const zp = ZegoUIKitPrebuilt.create(kitToken);
+
         zp.joinRoom({
             container: e,
-            showScreenSharingButton : false,
+            maxUsers: 25,
+            showAudioVideoSettingsButton: true,
+            showScreenSharingButton: true,
+            videoResolutionList: [
+                ZegoUIKitPrebuilt.VideoResolution_360P,
+                ZegoUIKitPrebuilt.VideoResolution_180P,
+                ZegoUIKitPrebuilt.VideoResolution_480P,
+                ZegoUIKitPrebuilt.VideoResolution_720P,
+              ],
+           videoResolutionDefault: ZegoUIKitPrebuilt.VideoResolution_720P, 
+
+            screenSharingConfig: {
+                resolution: ZegoUIKitPrebuilt.ScreenSharingResolution_2K
+            },
             scenario: {
                 mode: ZegoUIKitPrebuilt.GroupCall,
             },
+
             sharedLinks: [
                 {
-                  name: 'Copy Link',
-                  url:`localhost:5173/roomID/${idData.id}`
+                    name: 'Copy Link',
+                    url: `localhost:5173/roomID/${idData.id}`
                 },
-              ],
+            ],
         });
     }
     return (
         <div className='min-h-screen'>
-                    <div ref={meetings} />
+            <div ref={meetings} className='w-screen h-screen' />
         </div>
     )
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -17,7 +17,7 @@ const Homepage = () => {
     <div className=' flex flex-col m-5 items-center justify-center gap-3'>
       <input type="text" value={value} onChange={(e)=>setvalue(e.currentTarget.value)} placeholder='Create new meeting' className='md:w-1/3 w-3/4 px-5 py-3 outline-none border ' />
       {isTrue ? ( <p className='text-red-400 md:w-1/3 w-3/4 text-start'>Enter Some Text Above..</p>) : (<></>)}
-      <button onClick={handleSubmit} className='md:w-1/3 w-3/4 px-5 py-3 outline-none border-none  bg-sky-400/75 text-white rounded-md hover:bg-sky-400'>Join</button>
+      <button onClick={handleSubmit} className='md:w-1/3 w-3/4 px-5 py-3 outline-none border-none  bg-blue-500/75 text-white rounded-md hover:bg-blue-500'>Join</button>
     </div>
   )
 }
